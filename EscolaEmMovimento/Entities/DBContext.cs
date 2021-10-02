62fd9d2f23ac8626a6bee0dd7102aefd8e2bdcf6
Caíque Fortunato <caiquefortunato@ufmg.br>
2021-10-02 11:31:40 -0300

    inciando o teste prático
    Montagem inicial da estrutura do projeto da Escola em Movimento

---
+++ EscolaEmMovimento/Entities/DBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebService.Entities
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext()
+        {
+        }
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+    }
+}
